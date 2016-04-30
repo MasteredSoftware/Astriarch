c6f3b99a080541bd5bb7e1046e4c225efbaf691f
Matt Palmerlee <mpalmerlee@gmail.com>
2016-04-30 15:35:39 -0700

    For 1.6.2: changed spaceplatforms back to have an advantage over everything and limit scores based on action.

--- public/js/astriarch/astriarch_base.js
+++ public/js/astriarch/astriarch_base.js
@@ -1,4 +1,4 @@
-var Astriarch = Astriarch || {Version: '1.6.1', ClientGameModel:null, PlayerGameOptions: null, GameId: null};
+var Astriarch = Astriarch || {Version: '1.6.2', ClientGameModel:null, PlayerGameOptions: null, GameId: null};
 
 var module = module || {};
 module.exports = Astriarch;
