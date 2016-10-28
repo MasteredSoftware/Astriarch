bcbd6c5b04c31703b0f6d247a4c2883a33d3c5a2
Matt Palmerlee <mpalmerlee@gmail.com>
2016-10-27 19:36:21 -0700

    1.7.6 summary of changes: * Changed Improvements so that at most they only increase the effectiveness of one worker per improvement, made AI adjustments for this new improvement scheme * AI now explores more early game * AI now sends more ships when it knows there is a space platform on the enemy planet * New population on Dead planets default to miners instead of farmers

--- public/js/astriarch/astriarch_base.js
+++ public/js/astriarch/astriarch_base.js
@@ -1,4 +1,4 @@
-var Astriarch = Astriarch || {Version: '1.7.5', ClientGameModel:null, PlayerGameOptions: null, GameId: null};
+var Astriarch = Astriarch || {Version: '1.7.6', ClientGameModel:null, PlayerGameOptions: null, GameId: null};
 
 var module = module || {};
 module.exports = Astriarch;
