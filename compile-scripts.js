f74e6de1c1df6c876caf039f3d0ee4ab7f4bf8f3
Matt Palmerlee <mpalmerlee@gmail.com>
2018-10-26 11:51:25 -0700

    moving files around to work better for prettier

--- compile-scripts.js
+++ compile-scripts.js
@@ -1,30 +1,29 @@
-var exec = require('child_process').exec;
-var path = require('path');
+var exec = require("child_process").exec;
+var path = require("path");
 
-var clientFiles = require('./client_file_registry.js');
+var clientFiles = require("./client_file_registry.js");
 
+var executeClosureCompiler = function(outputFile, fileList, outputDebugFile) {
+  var command =
+    "java -jar closure_compiler" +
+    path.sep +
+    "compiler.jar --compilation_level SIMPLE_OPTIMIZATIONS --js_output_file " +
+    outputFile +
+    " ";
+  for (var i = 0; i < fileList.length; i++) {
+    command += "--js " + "./public/" + fileList[i] + " ";
+  }
 
-var executeClosureCompiler = function(outputFile, fileList, outputDebugFile){
+  command += "2> " + outputDebugFile;
 
-	var command = "java -jar closure_compiler" + path.sep + "compiler.jar --compilation_level SIMPLE_OPTIMIZATIONS --js_output_file " + outputFile + " ";
-	for(var i = 0; i < fileList.length; i++){
-		command += "--js " + "./public/" + fileList[i] + " ";
-	}
-
-	command += "2> " + outputDebugFile;
-
-	exec(command,
-		function (error, stdout, stderr) {
-			console.log('stdout: ' + stdout);
-			console.log('stderr: ' + stderr);
-			if (error !== null) {
-				console.log('exec error: ' + error);
-			}
-		}
-	);
-
+  exec(command, function(error, stdout, stderr) {
+    console.log("stdout: " + stdout);
+    console.log("stderr: " + stderr);
+    if (error !== null) {
+      console.log("exec error: " + error);
+    }
+  });
 };
 
-
-executeClosureCompiler("./public/js/astriarch.js", clientFiles.clientFilesInternal, "output-debug-i.txt");
-executeClosureCompiler("./public/js/external.js", clientFiles.clientFilesExternal, "output-debug-e.txt");
+executeClosureCompiler("./public/js/dist/astriarch.js", clientFiles.clientFilesInternal, "output-debug-i.txt");
+executeClosureCompiler("./public/js/dist/external.js", clientFiles.clientFilesExternal, "output-debug-e.txt");
